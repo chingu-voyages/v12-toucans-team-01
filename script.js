808fdb80b79f1ab2d8d217e756a1cf2df2726f67
jhfranco <5017473+jhfranco@users.noreply.github.com>
2019-10-03 13:42:58 -0400

    time script

---
+++ script.js
@@ -0,0 +1,10 @@
+var clockElement = document.getElementById('clock');
+
+function updateClock(clock) {
+	clock.innerHTML = new Date();
+	// .toLocaleTimeString();
+}
+
+setInterval(function() {
+	updateClock(clockElement);
+}, 1000);
