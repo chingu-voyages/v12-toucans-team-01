3a79cc88322577295d7612ecca7562195f26af9d
jffxfrnco <5017473+jhfranco@users.noreply.github.com>
2019-10-03 15:21:04 -0400

    added time and greeting based on time

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
 var clockElement = document.getElementById('clock');
 
 function updateClock(clock) {
-	clock.innerHTML = new Date();
-	// .toLocaleTimeString();
+	clock.innerHTML = new Date().toLocaleTimeString([], {
+		//for when we have 24 hr setting
+		// hour12: false
+		timeStyle: 'short'
+	});
 }
 
-setInterval(function() {
+setInterval(function () {
 	updateClock(clockElement);
 }, 1000);
+
+var greetingTimeOfDay = document.getElementById('timeOfDay')
+var currentDate = new Date();
+var currentHr = currentDate.getHours();
+
+if (currentHr < 12) {
+	greetingTimeOfDay.innerHTML = 'Morning'
+} else if (currentHr < 18) {
+	greetingTimeOfDay.innerHTML = 'Afternoon'
+} else {
+	greetingTimeOfDay.innerHTML = 'Evening'
+}
+
+var name = document.getElementById('userName')
+userName.innerHTML = 'person'
